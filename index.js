45a780c5ace1c0d9c70ce230eae2ac418282d944
GaneshLanka-16 <ganeshlanka16@gmail.com>
2026-05-08 11:56:46 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  // Write your code here.
+  <div className='container'>
+    <h1>congratulation</h1>
+    <div className='card'>
+      <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png' />
+      <h1>L Ganesh </h1>
+      <p>Amrita sai institute of science and technology. Vijayawada </p>
+
+      <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png' />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
